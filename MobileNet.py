97d9aee1341b54b56a46632300bcc77e992eec80
IdkwhatImD0ing <91146598+IdkwhatImD0ing@users.noreply.github.com>
2022-06-19 16:19:51 -0700

    Updated readme

--- MobileNet.py
+++ MobileNet.py
@@ -83,30 +83,34 @@ labeled_x_train = labeled_x_train / 255.0
 unlabeled_x_train = unlabeled_x_train / 255.0
 x_test = x_test / 255.0
 
-labeled_x_train, labeled_x_val, labeled_y_train, labeled_y_val = train_test_split(labeled_x_train, labeled_y_train, test_size = 0.1)
-
-
+labeled_x_train, labeled_x_val, labeled_y_train, labeled_y_val = train_test_split(
+    labeled_x_train, labeled_y_train, test_size=0.1)
 
 print(labeled_y_train.shape)
 print(unlabeled_y_train.shape)
 
-combined_x = np.concatenate((labeled_x_train, unlabeled_x_train), axis = 0)
-combined_y = np.concatenate((labeled_y_train, unlabeled_y_train), axis = 0)
+combined_x = np.concatenate((labeled_x_train, unlabeled_x_train), axis=0)
+combined_y = np.concatenate((labeled_y_train, unlabeled_y_train), axis=0)
 
 BATCH_SIZE = 4
 SHUFFLE_BUFFER_SIZE = 500
 
-labeled_train_dataset = tf.data.Dataset.from_tensor_slices((labeled_x_train, labeled_y_train))
-labeled_train_dataset = labeled_train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-unlabeled_train_dataset = tf.data.Dataset.from_tensor_slices((unlabeled_x_train, unlabeled_y_train))
-unlabeled_train_dataset = unlabeled_train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-train_dataset = tf.data.Dataset.zip((unlabeled_train_dataset, labeled_train_dataset)) 
-
-
+labeled_train_dataset = tf.data.Dataset.from_tensor_slices(
+    (labeled_x_train, labeled_y_train))
+labeled_train_dataset = labeled_train_dataset.shuffle(
+    SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+unlabeled_train_dataset = tf.data.Dataset.from_tensor_slices(
+    (unlabeled_x_train, unlabeled_y_train))
+unlabeled_train_dataset = unlabeled_train_dataset.shuffle(
+    SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+train_dataset = tf.data.Dataset.zip(
+    (unlabeled_train_dataset, labeled_train_dataset))
 
 print("Finished Parsing")
 
+
 class SemisupervisedModel(keras.Model):
+
     def __init__(self, model, entropy=False, lamda=0.1):
         super().__init__()
 
@@ -131,25 +135,33 @@ class SemisupervisedModel(keras.Model):
     def call(self, images, training=True):
         return self.model(images, training=training)
 
-    def semisupervised_loss(self, labeled_y_true, labeled_y_pred, unlabeled_y_true, unlabeled_y_pred):
+    def semisupervised_loss(self, labeled_y_true, labeled_y_pred,
+                            unlabeled_y_true, unlabeled_y_pred):
         loss = self.cce(labeled_y_true, labeled_y_pred)
         if self.entropy:
-            loss += -self.lamda * tf.reduce_mean(tf.reduce_sum(unlabeled_y_pred * tf.math.log(unlabeled_y_pred + 1e-12), axis=1))
+            loss += -self.lamda * tf.reduce_mean(
+                tf.reduce_sum(
+                    unlabeled_y_pred * tf.math.log(unlabeled_y_pred + 1e-12),
+                    axis=1))
         return loss
 
     def train_step(self, data):
-        (unlabeled_images, unlabeled_y_true), (labeled_images, labeled_y_true) = data
+        (unlabeled_images, unlabeled_y_true), (labeled_images,
+                                               labeled_y_true) = data
 
         with tf.GradientTape() as tape:
             labeled_y_pred = self.call(labeled_images, training=True)
             unlabeled_y_pred = self.call(unlabeled_images, training=True)
-            loss = self.semisupervised_loss(labeled_y_true, labeled_y_pred, unlabeled_y_true, unlabeled_y_pred)
+            loss = self.semisupervised_loss(labeled_y_true, labeled_y_pred,
+                                            unlabeled_y_true, unlabeled_y_pred)
         gradients = tape.gradient(loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
         self.loss_tracker.update_state(loss)
 
         #self.accuracy_tracker.update_state(tf.math.argmax(unlabeled_y_true, axis=1), tf.math.argmax(unlabeled_y_pred, axis=1)) #Only for if your unlabeled data has accurate labels.
-        self.accuracy_tracker.update_state(tf.math.argmax(labeled_y_true, axis=1), tf.math.argmax(labeled_y_pred, axis=1))
+        self.accuracy_tracker.update_state(
+            tf.math.argmax(labeled_y_true, axis=1),
+            tf.math.argmax(labeled_y_pred, axis=1))
 
         return {m.name: m.result() for m in self.metrics}
 
@@ -160,9 +172,11 @@ class SemisupervisedModel(keras.Model):
         loss = self.cce(labels, y_pred)
         self.loss_tracker.update_state(loss)
 
-        self.accuracy_tracker.update_state(tf.math.argmax(labels, axis=1), tf.math.argmax(y_pred, axis=1))
+        self.accuracy_tracker.update_state(tf.math.argmax(labels, axis=1),
+                                           tf.math.argmax(y_pred, axis=1))
 
         return {m.name: m.result() for m in self.metrics}
+
 
 mobilenet_v2 = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
 feature_extractor_model = mobilenet_v2
@@ -171,36 +185,35 @@ feature_extractor_layer = hub.KerasLayer(feature_extractor_model,
                                          input_shape=(224, 224, 3),
                                          trainable=False)
 
-semi_model = SemisupervisedModel(
-    keras.Sequential(
-    [
-        keras.layers.Resizing(224, 224),
-        feature_extractor_layer,
-        keras.layers.Flatten(),
-        keras.layers.Dense(256, activation='relu'),
-        keras.layers.Dropout(0.5),
-        keras.layers.BatchNormalization(),
-        keras.layers.Dense(6, activation="softmax"),
-    ]
-    ),
-    entropy=True
-)
+semi_model = SemisupervisedModel(keras.Sequential([
+    keras.layers.Resizing(224, 224),
+    feature_extractor_layer,
+    keras.layers.Flatten(),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dense(6, activation="softmax"),
+]),
+                                 entropy=True)
 
 NUM_EPOCHS = 20
 
-
-semi_model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.005), metrics=["accuracy"])
+semi_model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.005),
+                   metrics=["accuracy"])
 semi_model.build(input_shape=(None, 64, 64, 3))
 semi_model.summary()
 
-history = semi_model.fit(train_dataset, epochs= NUM_EPOCHS, validation_data = (labeled_x_val, labeled_y_val))
-
+history = semi_model.fit(train_dataset,
+                         epochs=NUM_EPOCHS,
+                         validation_data=(labeled_x_val, labeled_y_val))
 
 predictions = semi_model.predict(x_test, batch_size=32)
 y_test = np.argmax(predictions, axis=1)
 semi_model.evaluate(labeled_x_val, labeled_y_val)
 combined = [[i, j] for i, j in zip(img_id, y_test)]
 
+
+## Saving the results and model
 with open("mobilepredictions.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows([["image_id", "label"]])
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+# MedMnist Entropy-based Semi-Supervised Machine Learning
+Machine Learning model trained on a modified version of the MedMNIST dataset.
+
+## Competition Link
+https://www.kaggle.com/competitions/ucsc-cse-164-spring-2022-final-project/leaderboard?tab=public
+
+## Dataset
+This dataset is not the complete MedMNIST dataset. A subset of 6 different classes along with 60 labeled and around 50k unlabeled images were used to train this model.
+
+Each class had 10 labeled images. 
+
+Dataset is contained in MedMnist folder.
+
+## Training the Model
+The given labeled dataset was split into two parts, one for training and one for validation.
+
+## Results
+Transfer learning using MobileNet resulted in 99.5% testing accuracy.
+
+
+## How to reproduce the results
 Run the attached python file.
 Some modules may have to be installed ahead of time.
